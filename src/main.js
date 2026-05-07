5981dd5593b77f31da4d61f04c1c32acb79f0d88
Dev-GAF <guilhermefortesoffice@gmail.com>
2026-05-07 16:28:57 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,28 +1,107 @@
 const meses = [
-  "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-  "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+  "Janeiro", "Fevereiro", "Março", "Abril", 
+  "Maio", "Junho", "Julho"
 ];
 
-function criarMeses(meses) 
-{
-    const tbody = document.querySelector(".agenda table tbody");
-
-    for (const mes of meses) 
-    {
-        const linha = document.createElement("tr");
-        const dadoLinha = document.createElement("td");
-
-        dadoLinha.textContent = mes;
-
-        linha.appendChild(dadoLinha);
-        tbody.appendChild(linha);
-    }
-}
+const tdMeses = document.querySelectorAll(".mes");
 
 function criarCalendario(numeroMes) 
-{}
+{
+    const qtdDiasMes = new Date(2026, numeroMes + 1, 0).getDate();
+
+    const divTable = document.createElement("div");
+    divTable.id = "div-table-mes";
+
+    const table = document.createElement("table");
+
+    const tbody = document.createElement("tbody");
+    
+    let tr = document.createElement("tr");
+
+    for (let i=1; i<=qtdDiasMes; i++)
+    {
+        if (i==7) 
+        {
+            tr = document.createElement("tr");
+            tbody.appendChild(tr);
+        }      
+        
+        const td = document.createElement("td");
+        td.textContent = i;
+        tr.appendChild(td);
+    }
+
+    tbody.appendChild(tr);
+    table.appendChild(tbody);
+    divTable.appendChild(table);
+    tdMes.appendChild(divTable);
+}
 
 function abrirMes(linha, numeroMes) 
 {}
 
 criarMeses(meses);
+
+/*
+const meses = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho"
+];
+
+const tdMeses = document.querySelectorAll(".mes");
+
+tdMeses.forEach((td) => {
+    td.addEventListener("click", () => {
+        const numeroMes = Number(td.dataset.mes);
+
+        criarCalendario(td, numeroMes);
+    });
+});
+
+function criarCalendario(elementoMes, numeroMes)
+{
+    const calendarioExistente = elementoMes.querySelector(".div-table-mes");
+
+    if (calendarioExistente)
+    {
+        calendarioExistente.remove();
+        return;
+    }
+
+    const qtdDiasMes = new Date(2026, numeroMes + 1, 0).getDate();
+
+    const divTable = document.createElement("div");
+    divTable.classList.add("div-table-mes");
+
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+
+    let tr = document.createElement("tr");
+
+    for (let i = 1; i <= qtdDiasMes; i++)
+    {
+        if ((i - 1) % 7 === 0 && i !== 1)
+        {
+            tbody.appendChild(tr);
+            tr = document.createElement("tr");
+        }
+
+        const td = document.createElement("td");
+        td.textContent = i;
+
+        tr.appendChild(td);
+    }
+
+    tbody.appendChild(tr);
+
+    table.appendChild(tbody);
+    divTable.appendChild(table);
+
+    elementoMes.appendChild(divTable);
+}
+*/
