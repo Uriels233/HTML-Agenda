f67f2e4a8edec0957f3b42f1ef7f9e3f235b51bb
Dev-Gaf <guilhermefortesoffice@gmail.com>
2026-05-08 10:44:59 -0300

    feat(HTML-AGENDA) implementar calendário com os dias
    - Captura os dias do mês através do Date().
    - Implementa os dia da semana em cima dos dias.
    - Adiciona evento (addEventListener( , )) para capturar mês clicado pelo usuário.
    - Implementa lógica: se caso o calendário já existir, calendário é removido.
    - Cria uma nova linha após clicar no mês selecionado, onde é inserido o calendário (table).

--- src/main.js
+++ src/main.js
@@ -1,29 +1,60 @@
 const meses = [
-  "Janeiro", "Fevereiro", "Março", "Abril", 
-  "Maio", "Junho", "Julho"
+  "Janeiro", "Fevereiro", "Março", 
+  "Abril",   "Maio",      "Junho", 
 ];
+
+const diasDaSemana = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const tdMeses = document.querySelectorAll(".mes");
 
-function criarCalendario(numeroMes) 
+tdMeses.forEach((td) => {
+    td.addEventListener('click', () => {
+        const dateMes = Number(td.getAttribute("data-mes"));
+
+        criarCalendario(td, dateMes);
+    });
+});
+
+function criarCalendario(elementoMes, numeroMes) 
 {
+    const calendario = document.querySelector(".div-table-mes");
+
+    if (calendario)
+    {
+        calendario.remove();
+        return;
+    }
+
     const qtdDiasMes = new Date(2026, numeroMes + 1, 0).getDate();
 
     const divTable = document.createElement("div");
-    divTable.id = "div-table-mes";
+    divTable.classList.add("div-table-mes");
 
     const table = document.createElement("table");
 
+    const thead = document.createElement("thead");
     const tbody = document.createElement("tbody");
     
     let tr = document.createElement("tr");
 
+    diasDaSemana.forEach((dia) => {
+        const th = document.createElement("th");
+        th.textContent = dia;
+
+        tr.appendChild(th);
+    });
+
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    tr = document.createElement("tr");
+
     for (let i=1; i<=qtdDiasMes; i++)
     {
-        if (i==7) 
+        if ((i-1) % 7 === 0 && i !== 1) 
         {
-            tr = document.createElement("tr");
             tbody.appendChild(tr);
+            tr = document.createElement("tr");
         }      
         
         const td = document.createElement("td");
@@ -32,15 +63,24 @@ function criarCalendario(numeroMes)
     }
 
     tbody.appendChild(tr);
+
     table.appendChild(tbody);
+
     divTable.appendChild(table);
-    tdMes.appendChild(divTable);
+
+    const trMes = elementoMes.parentElement;
+
+    const trCalendario = document.createElement("tr");
+    trCalendario.classList.add("linha-calendario");
+
+    const tdCalendario = document.createElement("td");
+    tdCalendario.colSpan = 1;
+
+    tdCalendario.appendChild(divTable);
+    trCalendario.appendChild(tdCalendario);
+
+    trMes.insertAdjacentElement("afterend", trCalendario);
 }
-
-function abrirMes(linha, numeroMes) 
-{}
-
-criarMeses(meses);
 
 /*
 const meses = [
